49343aefe45d35a8728e1d4fa966b0f88460ba70
Aga <agnieszka.golan92@wp.pl>
2023-05-31 18:46:09 +0200

    push

---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Introduction-Topics/Introduction.htm'],s:['0','similar'],p:[['0',9],['every',5],['one',13],['they',4]]});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638211553801313968'
+});
